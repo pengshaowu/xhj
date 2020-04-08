6c8e8f8970a3ac44eafbc2f99bb1d60aa80c4654
[pengshaowu] <1454628711@qq.com>
2020-04-08 16:13:50 +0800

    提交首页图片

---
+++ js/home.js
@@ -0,0 +1,26 @@
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true, // 循环模式选项
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
+var mySwiper = new Swiper('.swiper-container2', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
+var mySwiper = new Swiper('.swiper-container3', {
+    slidesPerView: 2,
+    spaceBetween: 184,
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
