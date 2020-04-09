adf06fc0cb474a2769150ba7e6328e6fc7e5259a
[pengshaowu] <1454628711@qq.com>
2020-04-09 10:20:19 +0800

    提交关于我们

--- js/home.js
+++ js/home.js
@@ -1,5 +1,7 @@
 var mySwiper = new Swiper('.swiper-container', {
     loop: true, // 循环模式选项
+    speed:400, //切换时过渡时间
+    autoplay:true, //是否自动切换
     // 如果需要前进后退按钮
     navigation: {
         nextEl: '.swiper-button-next',
@@ -11,8 +13,8 @@ var mySwiper = new Swiper('.swiper-container2', {
     spaceBetween: 30,
     // 如果需要前进后退按钮
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next2',
+        prevEl: '.swiper-button-prev2',
     },
 })
 var mySwiper = new Swiper('.swiper-container3', {
@@ -20,7 +22,7 @@ var mySwiper = new Swiper('.swiper-container3', {
     spaceBetween: 184,
     // 如果需要前进后退按钮
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next3',
+        prevEl: '.swiper-button-prev3',
     },
 })
